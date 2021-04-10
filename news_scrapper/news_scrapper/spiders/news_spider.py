7682b874cd04a0afd52276a5fb6c51246f40a74a
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-04-10 23:21:24 +0430

    feat(items):add ContentItem class

--- news_scrapper/news_scrapper/spiders/news_spider.py
+++ news_scrapper/news_scrapper/spiders/news_spider.py
@@ -1,17 +1,15 @@
 import scrapy 
-
+from news_scrapper.items import ContentItem
 class NewsSpider(scrapy.Spider):
     name='news'
 
     def start_requests(self):
         urls = [
-            'https://www.nytimes.com/'
+            'https://english.elpais.com/'
         ]
         for url in urls:
-            yield scrapy.Request(url=url,callback=self.parse)
+            yield scrapy.Request(url=url,callback=self.parse_content)
         
-    def parse(self , response):
-        filename = 'test.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+    def parse_content(self , response):
+        for a in response.css('a::text').getall():
+            yield ContentItem(title=a)
