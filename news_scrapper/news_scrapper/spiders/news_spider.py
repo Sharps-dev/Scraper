4935e9892af6b71d0e790fc990b8806eb1365f2d
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-04-14 21:43:05 +0430

    feat(news_spider):add main function to run crawl process

--- news_scrapper/news_scrapper/spiders/news_spider.py
+++ news_scrapper/news_scrapper/spiders/news_spider.py
@@ -5,11 +5,12 @@ class NewsSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://english.elpais.com/'
+            'https://techcrunch.com/'
         ]
         for url in urls:
             yield scrapy.Request(url=url,callback=self.parse_content)
         
     def parse_content(self , response):
-        for a in response.css('a::text').getall():
+        for a in response.css('.post-block'):
+            title = a.css('.post-block__title__link::text').get()
             yield ContentItem(title=a)
