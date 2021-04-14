901d071f11b0942344e84cc4c8064927ebd94811
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-04-14 22:47:00 +0430

    feat(news_spider):add techcrunch news spider

--- news_scrapper/spiders/news_spider.py
+++ news_scrapper/spiders/news_spider.py
@@ -12,5 +12,8 @@ class NewsSpider(scrapy.Spider):
         
     def parse_content(self , response):
         for a in response.css('.post-block'):
-            title = a.css('.post-block__title__link::text').get()
-            yield ContentItem(title=a)
+            title = a.css('.post-block__header a.post-block__title__link::text').get()
+            url = a.css('.post-block__header a.post-block__title__link').attrib['href']
+            image_url = a.css('.post-block__media img').attrib['src']
+            description = a.css('.post-block__content::text').get()
+            yield ContentItem(title=title , url = url , image_url=image_url , description = description)
