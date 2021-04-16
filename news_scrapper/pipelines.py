5cb286477b3406d9a55ca75b7a3970b4d5be3236
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-04-16 22:28:08 +0430

    feat(itempipeline):add json writer pipeline

--- news_scrapper/pipelines.py
+++ news_scrapper/pipelines.py
@@ -6,8 +6,19 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from news_scrapper.settings import ITEM_OUTPUT_PATH
 
+import json
 
-class NewsScrapperPipeline:
+class JsonWriterPipeline:
+
+    def open_spider(self , spider):
+        self.file = open(ITEM_OUTPUT_PATH , 'w')
+
+    def close_spider(self , spide):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(ItemAdapter(item).asdict()) + '\n'
+        self.file.write(line)
         return item
